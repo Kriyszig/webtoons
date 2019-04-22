dc20950015faa48e7aed32c9228ef2611a6651a4
Kriyszig <lelouch.cpp@gmail.com>
2019-04-23 00:04:44 +0530

    Enhancement: Automated folder creation for new series and episodes

--- index.js
+++ index.js
@@ -1,47 +1,101 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 async function run(title, episode) {
-
+  /*
+   * Starting a headleess session of chromium with Puppeteer
+   * Note: Alter {headless: true} to {headless: flase} to see automated operations in action
+   * Setting Viewport to 1280 * 10000
+   * Note: 10000 was found to be a safe limit for long screenshot rendering. Somewhere between
+   * 10000px and 15000px, chrome fails to grab a complete screenshot of the page 
+   */
   let browser = await puppeteer.launch({headless: true});
   let page = await browser.newPage();
   await page.setViewport({width: 1280, height: 10000});
 
+  /*
+   * Setting the target webtoon URL.
+   * The url format is exactly as shown below. 
+   * On Line Webtoons, just changing the title_no and episode_no params will automatically
+   * redirect to the specified webtoon at the specified episode and implicitly changes the URL
+   * to match the webtoon 
+   */
   await page.goto(
       `https://www.webtoons.com/en/fantasy/tower-of-god/season-1-ep-0/viewer?title_no=${
           title}&episode_no=${episode}`);
-  await page.waitFor(2000);
-  
-  const height = await page.evaluate(() => {
+  await page.waitFor(2000); // Allowing time to complete render
 
+  /*
+   * As mentioned above, the URL changes dynamically.
+   * Splitting the URL at every "/" and comapring with the URL above, we see the new URL will
+   * give the name of the series at index 5
+   */
+  const url = await page.url();
+  const name = url.split('/')[5];
+
+  /*
+   * MAking sure directories exist to store the webtoon.
+   * Current saving path is of the format:
+   * ./webtoons/name_of_the_webtoon_series/episode_number/part_x.jpg
+   */
+  if(!fs.existsSync(`./webtoons/`)){
+    fs.mkdirSync(`./webtoons/`);
+  }
+  if(!fs.existsSync(`./webtoons/${name}/`)){
+    fs.mkdirSync(`./webtoons/${name}/`);
+  }
+  if(!fs.existsSync(`./webtoons/${name}/ep${episode}/`)){
+    fs.mkdirSync(`./webtoons/${name}/ep${episode}/`);
+  }
+
+  // Fetching the height of the webtoon
+  const height = await page.evaluate(() => {
+    /*
+     * At the end of every webtoon is an ad. This ad is bound to a HTML div with Id
+     * "viewerAdWrapper". Getting the offset of this div from top is same as finding 
+     * the net height of the webtoon
+     */
     let adDiv = document.getElementById('viewerAdWrapper')
     if (adDiv != null) {
       return adDiv.offsetTop
     }
-
+    
+    /*
+     * In case there exists no ad (weird!), we approximate the webtoon to end 4800px from
+     * the bottom of the page.
+     */
     return document.documentElement.scrollHeight - 4800;
   });
 
+  /*
+   * Iterating over the entire height of webtoon and capturing screenshots of size of viewport
+   * in chunks until we finally capture the entire webtoon
+   */
   for (let i = 0; i < height; i += 10000) {
-
+    /*
+     * For the final chunk, the height may not be equal to the height of the current viewport.
+     * Hence changing height to fit the final part exactly within the screen
+     */
     if (height - i < 10000) {
       await page.setViewport({width: 1280, height: height - i});
     }
-
-    await page.waitFor(1000);
+    await page.waitFor(1000); // Awaiting render after scrolldown action
     await page.screenshot({
-      path: `./webtoons/lookism/ep${episode}/p${Math.floor(i / 10000) + 1}.jpg`,
+      path: `./webtoons/${name}/ep${episode}/p${Math.floor(i / 10000) + 1}.jpg`,
       type: 'jpeg'
     });
 
+    // No need to scroll after the final part
     if (height - i >= 10000) {
       await page.evaluate(() => {
         document.documentElement.scrollBy(0, 10000);
       });
     }
   }
-  
+
+  // Closing page and the chromium session
   await page.close();
   await browser.close();
 }
 
-run(1049, 1);
+run(95, 2);
